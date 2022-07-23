ad0fcb5fb65eb1b943ecd4430e1a349738f749f8
HP-OMEN\ruben <carmonagarcia@gmail.com>
2022-07-23 10:24:00 +0200

    Iniciamos repositorio y estructura.

---
+++ src/main/java/com/rubencarmona/microservicecalculator/MicroservicecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.rubencarmona.microservicecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubencarmona/microservicecalculator/MicroservicecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubencarmona.microservicecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicecalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
